f82fe078b4a0128385b3ddce3d7773bc1176ebdf
Sebastian Ruiz <sbstn540@gmail.com>
2020-04-09 23:02:26 -0700

    Removed npm ignore, edited gitignore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { LinksComponent } from './links/links.component';
 import { AboutComponent } from './about/about.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { ReactiveFormsModule } from '@angular/forms';
+
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,8 +16,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
-    // HttpClientModule,
-    // ReactiveFormsModule
   ],
   exports: [RouterModule]
 })
